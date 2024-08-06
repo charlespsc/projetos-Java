f66be9486945f59a48513e934242229ac4d3fe15
Charles Pereira <charlespereira.fln@gmail.com>
2024-08-06 10:11:05 -0300

    While

---
+++ futuroDEV-While/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int numero;
+
+        numero = 1;
+
+        while (numero <= 5) {
+            System.out.println("Valor: " + numero);
+            //numero = numero + 1; (não usamos mais assim!)
+            numero++;
+        }
+    }
+}
