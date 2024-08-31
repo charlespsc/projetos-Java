ba558241595546d007fbb55e3623051e0dc93d51
Charles Pereira <charlespereira.fln@gmail.com>
2024-08-31 16:59:08 -0300

    Função com parâmetros

--- futuroDEV-func/src/Main.java
+++ futuroDEV-func/src/Main.java
@@ -2,12 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Antes da função...");
+        System.out.println("Mostrando mensagem da função:");
         mostratMensagem();
-        System.out.println("Depois da função!");
+
+        System.out.println("Função somaValores, passando parâmetros:");
+        somaValores(5, 9);
     }
 
     public static void mostratMensagem() {
         System.out.println("Minha primeira função!");
+    }
+
+    public static void somaValores(int valor1, int valor2) {
+        int soma = valor1 + valor2;
+        System.out.println(soma);
     }
 }
