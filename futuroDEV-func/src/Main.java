1f13888cd831a7ad3d88982e0b045cc96b26e1f4
Charles Pereira <charlespereira.fln@gmail.com>
2024-08-31 17:06:25 -0300

    Função com retorno

--- futuroDEV-func/src/Main.java
+++ futuroDEV-func/src/Main.java
@@ -2,19 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mostrando mensagem da função:");
-        mostratMensagem();
-
-        System.out.println("Função somaValores, passando parâmetros:");
-        somaValores(5, 9);
-    }
-
-    public static void mostratMensagem() {
-        System.out.println("Minha primeira função!");
-    }
-
-    public static void somaValores(int valor1, int valor2) {
-        int soma = valor1 + valor2;
+        int soma = somaValores(5, 9);
         System.out.println(soma);
+    }
+
+    public static int somaValores(int valor1, int valor2) {
+        int soma = valor1 + valor2;
+        return soma;
     }
 }
