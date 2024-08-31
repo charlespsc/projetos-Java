5740c37b4151ca2f8053f79e5029c2803d9cfa7a
Charles Pereira <charlespereira.fln@gmail.com>
2024-08-31 15:51:16 -0300

    Funções

---
+++ futuroDEV-func/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Antes da função...");
+        mostratMensagem();
+        System.out.println("Depois da função!");
+    }
+
+    public static void mostratMensagem() {
+        System.out.println("Minha primeira função!");
+    }
+}
